5fbfb305c7333527e8ff68aade1c18a5ea7ba2eb
chaksaray <chaksaray@gmail.com>
2016-04-08 15:55:13 +0700

    analytic

--- safirv1/client/controllers/logout.js
+++ safirv1/client/controllers/logout.js
@@ -1,7 +1,7 @@
 Template.header.events({
     'click #logout': function(event){
         event.preventDefault();
-        var url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-71059459-2&cid=888&ec=Logout&ea=click&el=logout&ev=1';
+        var url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-71059459-2&cid=555&ec=Logout&ea=click&el=logout&ev=1';
         Meteor.call('eventCall', url, function(error, result) {
             if (error) {
                 console.log('Analytic CLIENT ERRR');
